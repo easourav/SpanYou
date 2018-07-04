97d91894456e783943989552485439fa900c5393
easourav <37806949+easourav@users.noreply.github.com>
2018-07-05 02:00:59 +0600

    set condition all the set of the degree of the wheel.

--- app/src/main/java/com/example/sourav/spanyou/MainActivity.java
+++ app/src/main/java/com/example/sourav/spanyou/MainActivity.java
@@ -77,12 +77,13 @@ public class MainActivity extends AppCompatActivity {
         rotateAnimation.setAnimationListener(new Animation.AnimationListener() {
             @Override
             public void onAnimationStart(Animation animation) {
+                player1TV.setText("");
 
             }
 
             @Override
             public void onAnimationEnd(Animation animation) {
-
+                player1TV.setText(currentNumber(360-(degree%360)));
             }
 
             @Override
@@ -91,5 +92,121 @@ public class MainActivity extends AppCompatActivity {
             }
         });
         wheelIMG.startAnimation(rotateAnimation);
+    }
+    private String currentNumber(int degrees){
+        String text ="";
+        if ((degrees>=(factor*1) && degrees < (factor*3))){
+            text="22 Black";
+        }
+        if ((degrees>=(factor*3) && degrees < (factor*5))){
+            text="18 Red";
+        }
+        if ((degrees>=(factor*5) && degrees < (factor*7))){
+            text="29 Black";
+        }
+        if ((degrees>=(factor*7) && degrees < (factor*9))){
+            text="7 Red";
+        }
+        if ((degrees>=(factor*9) && degrees < (factor*11))){
+            text="28 Black";
+        }
+        if ((degrees>=(factor*11) && degrees < (factor*13))){
+            text="12 Red";
+        }
+        if ((degrees>=(factor*13) && degrees < (factor*15))){
+            text="35 Black";
+        }
+        if ((degrees>=(factor*15) && degrees < (factor*17))){
+            text="3 Red";
+        }
+        if ((degrees>=(factor*17) && degrees < (factor*19))){
+            text="26 Black";
+        }
+        if ((degrees>=(factor*19) && degrees < (factor*21))){
+            text="0 Green";
+        }
+        if ((degrees>=(factor*21) && degrees < (factor*23))){
+            text="32 Red";
+        }
+        if ((degrees>=(factor*23) && degrees < (factor*25))){
+            text="15 Black";
+        }
+        if ((degrees>=(factor*25) && degrees < (factor*27))){
+            text="19 Red";
+        }
+        if ((degrees>=(factor*27) && degrees < (factor*29))){
+            text="4 Black";
+        }
+        if ((degrees>=(factor*29) && degrees < (factor*31))){
+            text="21 Red";
+        }
+        if ((degrees>=(factor*31) && degrees < (factor*33))){
+            text="2 Black";
+        }
+        if ((degrees>=(factor*33) && degrees < (factor*35))){
+            text="25 Red";
+        }
+        if ((degrees>=(factor*35) && degrees < (factor*37))){
+            text="17 Black";
+        }
+        if ((degrees>=(factor*37) && degrees < (factor*39))){
+            text="34 Red";
+        }
+        if ((degrees>=(factor*39) && degrees < (factor*41))){
+            text="6 Black";
+        }
+        if ((degrees>=(factor*41) && degrees < (factor*43))){
+            text="27 Red";
+        }
+        if ((degrees>=(factor*43) && degrees < (factor*45))){
+            text="13 Black";
+        }
+        if ((degrees>=(factor*45) && degrees < (factor*47))){
+            text="36 Red";
+        }
+        if ((degrees>=(factor*47) && degrees < (factor*49))){
+            text="11 Black";
+        }
+        if ((degrees>=(factor*49) && degrees < (factor*51))){
+            text="30 Red";
+        }
+        if ((degrees>=(factor*51) && degrees < (factor*53))){
+            text="8 Black";
+        }
+        if ((degrees>=(factor*53) && degrees < (factor*55))){
+            text="23 Red";
+        }
+        if ((degrees>=(factor*55) && degrees < (factor*57))){
+            text="10 Black";
+        }
+        if ((degrees>=(factor*57) && degrees < (factor*59))){
+            text="5 Red";
+        }
+        if ((degrees>=(factor*59) && degrees < (factor*61))){
+            text="24 Black";
+        }
+        if ((degrees>=(factor*61) && degrees < (factor*63))){
+            text="16 Red";
+        }
+        if ((degrees>=(factor*63) && degrees < (factor*65))){
+            text="33 Black";
+        }
+        if ((degrees>=(factor*65) && degrees < (factor*67))){
+            text="1 Red";
+        }
+        if ((degrees>=(factor*67) && degrees < (factor*69))){
+            text="20 Black";
+        }
+        if ((degrees>=(factor*69) && degrees < (factor*71))){
+            text="14 Red";
+        }
+        if ((degrees>=(factor*71) && degrees < (factor*73))){
+            text="31 Black";
+        }
+        if ((degrees>=(factor*73) && degrees < 360) || (degrees>= 0 && degrees<(factor*1))){
+            text="9 Red";
+        }
+
+        return text;
     }
 }
